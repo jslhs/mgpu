e7df6b5142f0b803fa6948067e02e0ff14d7982f
jslhs <aclazycat@gmail.com>
2014-05-23 20:48:26 +0800

    dxgi 1.2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,44 @@
 #include <Windows.h>
-#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <d3d11.h>
+
 #include <iostream>
 #include <vector>
 
 #pragma comment(lib, "dxgi")
+#pragma comment(lib, "d3d11")
 
 int main()
 {
-	IDXGIFactory1 *pDXGIFactory = nullptr;
-	auto hr = CreateDXGIFactory1(__uuidof(IDXGIFactory1), (void **)&pDXGIFactory);
-	if (FAILED(hr)) std::cout << "unable to create IDXGIFactory1!" << std::endl;
+	IDXGIFactory2 *pDXGIFactory = nullptr;
+	auto hr = CreateDXGIFactory1(__uuidof(IDXGIFactory2), (void **)&pDXGIFactory);
+	if (FAILED(hr)) std::cout << "unable to create IDXGIFactory2!" << std::endl;
 
 	UINT i = 0;
-	IDXGIAdapter1 *pAdapter;
-	std::vector<IDXGIAdapter1 *> adapters;
-	while (pDXGIFactory->EnumAdapters1(i++, &pAdapter) != DXGI_ERROR_NOT_FOUND)
+	IDXGIAdapter2 *pAdapter;
+	ID3D11Device *pDevice;
+	std::vector<IDXGIAdapter2 *> adapters;
+	std::vector<ID3D11Device *> devices;
+	while (pDXGIFactory->EnumAdapters1(i++, (IDXGIAdapter1 **)&pAdapter) != DXGI_ERROR_NOT_FOUND)
 	{
 		adapters.push_back(pAdapter);
-		DXGI_ADAPTER_DESC1 desc;
-		if (SUCCEEDED(pAdapter->GetDesc1(&desc)))
+		DXGI_ADAPTER_DESC2 desc;
+		if (SUCCEEDED(pAdapter->GetDesc2(&desc)))
 		{
 			if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) continue; // skip software adapter
 			std::cout << "Adapter[" << i << "]: ";
 			std::wcout << desc.Description << std::endl;
 		}
 	}
+
+	pAdapter = adapters.front();
+
+	D3D_FEATURE_LEVEL level;
+	D3D11CreateDevice(pAdapter, D3D_DRIVER_TYPE_UNKNOWN, nullptr, 0, 0, 0, D3D11_SDK_VERSION, &pDevice, &level, 0);
+
+ 	pDevice->Release();
+	pAdapter->Release();
+	pDXGIFactory->Release();
 
 	std::cin.ignore(1);
 	return 0;
