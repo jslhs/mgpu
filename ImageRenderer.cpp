0e5d503c19d8fc73266abd777271f41af7edbfa2
jslhs <aclazycat@gmail.com>
2014-06-30 18:33:50 +0800

    image renderer

--- ImageRenderer.cpp
+++ ImageRenderer.cpp
@@ -36,15 +36,19 @@ void ImageRenderer::init()
 	};
 
 	UINT flags[] = 
-	{ 
+	{
+#if _DEBUG
 		D3D11_CREATE_DEVICE_DEBUG, 
+#endif
 		0 
 	};
 
 	D3D_FEATURE_LEVEL levels[] =
 	{
 		D3D_FEATURE_LEVEL_11_1,
-		D3D_FEATURE_LEVEL_11_0
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0
 	};
 
 	for (auto &type : driveTypes)
@@ -55,8 +59,8 @@ void ImageRenderer::init()
 				, type
 				, nullptr
 				, flag
-				, levels
-				, ARRAYSIZE(levels)
+				, nullptr
+				, 0
 				, D3D11_SDK_VERSION
 				, &scd
 				, &_swap_chain
@@ -115,8 +119,8 @@ void ImageRenderer::init()
 	sf.set_source(ss);
 	//sf.set_source(std::ifstream("image.fx"));
 	sf.set_flags1(D3DCOMPILE_DEBUG | D3DCOMPILE_ENABLE_STRICTNESS);
-	_hr = sf.create_pixel_shader(&_ps, "ps_5_0", "ps_main");
-	_hr = sf.create_vertex_shader(&_vs, "vs_5_0", "vs_main");
+	_hr = sf.create_pixel_shader(&_ps, "ps_4_0", "ps_main");
+	_hr = sf.create_vertex_shader(&_vs, "vs_4_0", "vs_main");
 	_hr = sf.create_input_layout(ils, ARRAYSIZE(ils), &_layout);
 
 	D3D11_SAMPLER_DESC sd = {};
@@ -154,7 +158,86 @@ void ImageRenderer::init()
 
 	_hr = _dev->CreateBlendState(&blend_desc, &_blend);
 
+	D3D11_DEPTH_STENCIL_DESC ds_desc = {};
+	ds_desc.DepthEnable = FALSE;
+	ds_desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	ds_desc.DepthFunc = D3D11_COMPARISON_LESS;
+	ds_desc.StencilEnable = FALSE;
+	ds_desc.StencilReadMask = 0xFF;
+	ds_desc.StencilWriteMask = 0xFF;
+	ds_desc.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	ds_desc.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	ds_desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	ds_desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+	ds_desc.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	ds_desc.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	ds_desc.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	ds_desc.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	_hr = _dev->CreateDepthStencilState(&ds_desc, &_depth);
+
 	resizeBuffer();
+}
+
+void ImageRenderer::Init(Scene &scene)
+{
+	for(auto &s : scene._sprites)
+	{
+		float x = s.pos.x;
+		float y = s.pos.y;
+		float w = s.size.x;
+		float h = s.size.y;
+		vertex v[]=
+		{
+			/*
+			vertex(vec3(x + w, y + h, 1.0f), vec2(1.0f, 0.0f)),
+			vertex(vec3(x + w, y - h, 1.0f), vec2(1.0f, 1.0f)),
+			vertex(vec3(x - w, y - h, 1.0f), vec2(0.0f, 1.0f)),
+			vertex(vec3(x - w, y - h, 1.0f), vec2(0.0f, 1.0f)),
+			vertex(vec3(x - w, y + h, 1.0f), vec2(0.0f, 0.0f)),
+			vertex(vec3(x + w, y + h, 1.0f), vec2(1.0f, 0.0f)),
+			*/
+			vertex(vec3(x + w, y	, 1.0f), vec2(1.0f, 0.0f)),
+			vertex(vec3(x + w, y + h, 1.0f), vec2(1.0f, 1.0f)),
+			vertex(vec3(x	 , y + h, 1.0f), vec2(0.0f, 1.0f)),
+			vertex(vec3(x	 , y + h, 1.0f), vec2(0.0f, 1.0f)),
+			vertex(vec3(x	 , y	, 1.0f), vec2(0.0f, 0.0f)),
+			vertex(vec3(x + w, y	, 1.0f), vec2(1.0f, 0.0f)),
+		};
+
+		DrawItem item;
+		item.sprite = &s;
+		item.vertex_count = ARRAYSIZE(v);
+
+		D3D11_BUFFER_DESC vbd = {};
+		vbd.Usage = D3D11_USAGE_DEFAULT;
+		vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		vbd.ByteWidth = sizeof(v);
+
+		D3D11_SUBRESOURCE_DATA res = {};
+		res.pSysMem = v;
+		_hr = _dev->CreateBuffer(&vbd, &res, &item.vb);
+
+		D3D11_TEXTURE2D_DESC desc = {};
+		desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		desc.ArraySize = 1;
+		desc.Width = s.width;
+		desc.Height = s.height;
+		desc.SampleDesc.Count = 1;
+		desc.Format = s.no_alpha ? DXGI_FORMAT_B8G8R8X8_UNORM : DXGI_FORMAT_B8G8R8A8_UNORM;
+		desc.Usage = D3D11_USAGE_DEFAULT;
+		//desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		desc.MipLevels = 1;
+		//desc.MiscFlags = 0;//D3D11_RESOURCE_MISC_GENERATE_MIPS;
+
+		res.pSysMem = s.img;
+		res.SysMemPitch = s.width * 4;
+		//_hr = _dev->CreateTexture2D(&desc, nullptr, &item.tex);
+		_hr = _dev->CreateTexture2D(&desc, &res, &item.tex);
+		_hr = _dev->CreateShaderResourceView(item.tex, nullptr, &item.tex_view);
+
+		_items.push_back(std::move(item));
+	}
 }
 
 void ImageRenderer::resizeBuffer()
@@ -190,6 +273,38 @@ void ImageRenderer::resizeBuffer()
 	//_proj_m = XMMatrixTranspose(XMMatrixOrthographicOffCenterLH(-w, w, -h, h, 0.1f, 1.0f));
 	_proj_m = XMMatrixTranspose(XMMatrixOrthographicOffCenterLH(0.0f, w, h, 0.0f, 0.1f, 1.0f));
 	_ctx->UpdateSubresource(_cb_proj_m, 0, nullptr, &_proj_m, 0, 0);
+}
+
+void ImageRenderer::Render()
+{
+	float color[4] = {};
+	_ctx->ClearRenderTargetView(_back_buf_target, color);
+
+	for(auto &item : _items)
+	{
+		UINT row_pitch = item.sprite->width * 4;
+		UINT depth_pitch = row_pitch * item.sprite->height;
+		_ctx->UpdateSubresource(item.tex, 0, nullptr, item.sprite->img, row_pitch, depth_pitch);
+
+		//com_ptr<ID3D11ShaderResourceView> tex_view;
+		//_dev->CreateShaderResourceView(item.tex, nullptr, &tex_view);
+
+		UINT stride = sizeof(vertex);
+		UINT offset = 0;
+		_ctx->IASetInputLayout(_layout);
+		_ctx->IASetVertexBuffers(0, 1, &item.vb, &stride, &offset);
+		_ctx->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		_ctx->VSSetConstantBuffers(0, 1, &_cb_proj_m);
+		_ctx->VSSetShader(_vs, nullptr, 0);
+		_ctx->PSSetShader(_ps, nullptr, 0);
+		_ctx->PSSetSamplers(0, 1, &_sampler);
+		_ctx->PSSetShaderResources(0, 1, &item.tex_view);
+		_ctx->OMSetBlendState(_blend, nullptr, 0xffffffff);
+		_ctx->OMSetDepthStencilState(_depth, 1);
+		_ctx->Draw(item.vertex_count, 0);
+	}
+
+	_hr = _swap_chain->Present(0, 0);
 }
 
 void ImageRenderer::Render(const Scene &scene)
@@ -237,9 +352,9 @@ void ImageRenderer::Render(const Scene &scene)
 		desc.Width = s.width;
 		desc.Height = s.height;
 		desc.SampleDesc.Count = 1;
-		desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-		desc.Usage = D3D11_USAGE_DYNAMIC;
-		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		desc.Format = s.no_alpha ? DXGI_FORMAT_B8G8R8X8_UNORM : DXGI_FORMAT_B8G8R8A8_UNORM;
+		desc.Usage = D3D11_USAGE_DEFAULT;
+		//desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 		desc.MipLevels = 1;
 
 		com_ptr<ID3D11Texture2D> tex;
@@ -260,6 +375,7 @@ void ImageRenderer::Render(const Scene &scene)
 		_ctx->PSSetSamplers(0, 1, &_sampler);
 		_ctx->PSSetShaderResources(0, 1, &tex_view);
 		_ctx->OMSetBlendState(_blend, nullptr, 0xffffffff);
+		_ctx->OMSetDepthStencilState(_depth, 1);
 
 		_ctx->Draw(ARRAYSIZE(v), 0);
 	}
--- ImageRenderer.h
+++ ImageRenderer.h
@@ -18,12 +18,13 @@ public:
 	{
 	}
 
-	void Draw(float x, float y, float rw, float rh, void *img, unsigned int width, unsigned int height)
+	void Draw(float x, float y, float rw, float rh, void *img, unsigned int width, unsigned int height, bool no_alpha = false)
 	{
 		Sprite s;
 		s.pos = vec2(x, y);
 		s.size = vec2(rw, rh);
 		s.img = img;
+		s.no_alpha = no_alpha;
 		s.width = width;
 		s.height = height;
 		_sprites.push_back(s);
@@ -45,6 +46,7 @@ private:
 		vec2 pos;
 		vec2 size;
 		void *img;
+		bool no_alpha;
 		unsigned int width;
 		unsigned int height;
 	};
@@ -63,14 +65,49 @@ public:
 
 public:
 	//void Update(void *buf, unsigned int width, unsigned int height);
-	//void Init(Scene &s);
+	void Init(Scene &scene);
 	void Render(const Scene &s);
+	void Render();
 	void ResizeBuffer();
 
 private:
 	void init();
 	void resizeBuffer();
 	void createTexture(unsigned int width, unsigned int height);
+
+	struct DrawItem
+	{
+		Scene::Sprite *sprite;
+		int vertex_count;
+		com_ptr<ID3D11Buffer> vb;
+		com_ptr<ID3D11Texture2D> tex;
+		com_ptr<ID3D11ShaderResourceView> tex_view;
+
+		DrawItem()
+			: sprite(nullptr)
+			, vertex_count(0)
+		{
+		}
+
+		DrawItem(DrawItem &&item)
+		{
+			*this = std::move(item);
+		}
+
+		DrawItem &operator=(DrawItem &&item)
+		{
+			if(this != &item)
+			{
+				sprite = item.sprite;
+				vertex_count = item.vertex_count;
+				vb = std::move(item.vb);
+				tex = std::move(item.tex);
+				tex_view = std::move(item.tex_view);
+			}
+
+			return *this;
+		}
+	};
 	
 private:
 	HWND _hwnd;
@@ -89,10 +126,12 @@ private:
 	com_ptr<ID3D11PixelShader> _ps;
 	com_ptr<ID3D11SamplerState> _sampler;
 	com_ptr<ID3D11BlendState> _blend;
-
+	com_ptr<ID3D11DepthStencilState> _depth;
 	//com_ptr<ID3D11Buffer> _vb;
 	//com_ptr<ID3D11Texture2D> _tex;
 	//com_ptr<ID3D11ShaderResourceView> _tex_view;
 	DirectX::XMMATRIX _proj_m;
 	com_ptr<ID3D11Buffer> _cb_proj_m;
+
+	std::vector<DrawItem> _items;
 };
